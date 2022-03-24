b25699d9ee62115d6b2a99aea645c9f0db71ef8c
WongSilver <783244553@qq.com>
2022-03-24 15:32:41 +0800

    "test"

--- src/main.js
+++ src/main.js
@@ -1,12 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import ElementUI from "element-ui";
+import "./styles.scss";
+import axios from "axios";
 
-Vue.config.productionTip = false
+// 全局使用ele
+Vue.use(ElementUI);
+
+Vue.config.productionTip = false;
+// 添加http请求库
+Vue.prototype.$axios = axios;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
