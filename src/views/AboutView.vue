36c70d6ca66a8dae94bf20c6c34c1a4427a4a6d2
WongSilver <783244553@qq.com>
2022-03-25 15:26:08 +0800

    删除无用页面

--- src/views/AboutView.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
-     <el-button round>圆角按钮</el-button>
-       <el-button type="info" round>信息按钮</el-button>
-  </div>
-</template>
