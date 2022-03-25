493b052e9c29e6e8f90ddb9202547a1e4d68ecdd
WongSilver <783244553@qq.com>
2022-03-25 15:25:32 +0800

    添加store，设置一些值存储在本地

--- src/store/index.js
+++ src/store/index.js
@@ -5,13 +5,35 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    token: localStorage.getItem("token"),
+    userInfo: JSON.parse(sessionStorage.getItem("userInfo"))
   },
   getters: {
+    // 相当于Java里的get
+    getUserInfo: state => {
+      return state.userInfo
+    },
+    getToken:state=>{
+      return state.token
+    }
   },
   mutations: {
+    // 相当于Java里的set
+    SET_TOKEN: (state, token) => {
+      state.token = token
+      localStorage.setItem("token", token)
+    },
+    SET_USERINFO: (state, userInfo) => {
+      state.userInfo = userInfo
+      sessionStorage.setItem("userInfo", JSON.stringify(userInfo)) //序列化存入浏览器会话中
+    },
+    REMOVE_INFO(state) {
+      state.token = ''
+      state.userInfo = {}
+      localStorage.removeItem("token")
+      sessionStorage.removeItem("userInfo")
+    }
   },
-  actions: {
-  },
-  modules: {
-  }
+  actions: {},
+  modules: {}
 })
