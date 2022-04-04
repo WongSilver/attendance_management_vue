f583fd82b5edf612495967bb6a18cefd94f491d6
WongSilver <783244553@qq.com>
2022-04-04 22:45:05 +0800

    添加Home.vue

--- src/views/pages/Home.vue
+++ src/views/pages/Home.vue
@@ -1,9 +1,13 @@
 <template>
-  <div class="home">Home</div>
+  <div>HOME</div>
 </template>
 
 <script>
 export default {
-  name: "Home",
-};
+  name: "Home"
+}
 </script>
+
+<style scoped>
+
+</style>
