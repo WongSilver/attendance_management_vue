d6b37a47242bfc44dce849867ddb679e25496443
WongSilver <783244553@qq.com>
2022-04-04 22:48:04 +0800

    修改一些路由文件

--- src/router/index.js
+++ src/router/index.js
@@ -1,50 +1,41 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import Manage from "../views/pages/Manage.vue";
 import Home from "../views/pages/Home.vue";
 import Login from "../views/pages/Login.vue";
 import User from "../views/pages/User.vue";
-import UserDetail from "../views/pages/UserDetail.vue";
-import UserEdit from "../views/pages/UserEdit.vue";
+import UserDataShow from "../views/pages/UserDataShow.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: "/",
-    name: "Index",
-    redirect: { name: "Home" },
-  },
-  {
-    path: "/home",
-    name: "Home",
-    component: Home,
-  },
-  {
-    path: "/login",
-    name: "Login",
-    component: Login,
-  },
-  {
-    path: "/user/list",
-    name: "User",
-    component: User,
-  },
-  {
-    path: "/user/:userId",
-    name: "UserDetail",
-    component: UserDetail,
-  },
-  {
-    path: "/user/:userId/edit",
-    name: "UserEdit",
-    component: UserEdit,
-  },
+    {
+        path: "/",
+        name: "Manage",
+        component: Manage,
+        redirect: "/home",
+        children: [
+            {path: "home", name: "Home", component: Home},
+            {path: "user", name: "User", component: User},
+            {path: "userDataShow", name: "UserDataShow", component: UserDataShow},
+        ]
+    },
+    {
+        path: "/home",
+        name: "Home",
+        component: Home,
+    },
+    {
+        path: "/login",
+        name: "Login",
+        component: Login,
+    },
 ];
 
 const router = new VueRouter({
-  mode: "history", //去除#号键
-  // mode: "hash",
-  routes,
+    mode: "history", //去除#号键
+    // mode: "hash",
+    routes,
 });
 
 export default router;
