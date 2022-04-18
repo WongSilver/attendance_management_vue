83890998d8f91821bcceb1551d3ae796c622e2d0
WongSilver <783244553@qq.com>
2022-04-18 08:57:40 +0800

    refactor: 动态加载菜单

--- src/components/Aside.vue
+++ src/components/Aside.vue
@@ -14,66 +14,17 @@
            style="width: 20px; position: relative; top: 3px; margin-right: 5px">
       <b style="color: #c6e6e8" v-show="!isCollapse">考勤管理系统</b>
     </div>
-    <!--    主页-->
-    <el-menu-item index="/">
-      <i :style="{color:iconColor}" class="el-icon-house"></i>
-      <span slot="title">主页</span>
-    </el-menu-item>
 
-    <!--    用户管理-->
-    <el-submenu style="color: #c6e6e8" index="">
-      <template slot="title">
-        <i :style="{color:iconColor}" class="el-icon-user"></i>
-        <span slot="title">用户管理</span>
-      </template>
-      <el-menu-item index="user">用户列表</el-menu-item>
-      <el-menu-item index="userDataShow">用户分析</el-menu-item>
-    </el-submenu>
 
-    <!--    角色管理-->
-    <el-submenu index="3">
-      <template slot="title">
-        <i :style="{color:iconColor}" class="el-icon-link"></i>
-        <span slot="title">角色管理</span>
-      </template>
-      <el-menu-item index="/role">角色列表</el-menu-item>
-      <el-menu-item index="/roleDataShow">角色分析</el-menu-item>
-      <!--      <el-menu-item index="3-3">选项3</el-menu-item>
-            <el-submenu index="3-4">
-              <template slot="title">选项4</template>
-              <el-menu-item index="3-4-1">选项4-1</el-menu-item>
-            </el-submenu>-->
-    </el-submenu>
+    <!--    TODO-->
+    <div v-for="item in rights" :key="item.id">
+      <el-menu-item :index="item.url">
+        <i :style="{color:iconColor}" :class="item.icon"></i>
+        <span slot="title">{{ item.name }}</span>
+      </el-menu-item>
+    </div>
 
-    <!--    权限管理-->
-    <el-submenu index="4">
-      <template slot="title">
-        <i :style="{color:iconColor}" class="el-icon-key"></i>
-        <span slot="title">权限管理</span>
-      </template>
-      <el-menu-item index="/right">权限列表</el-menu-item>
-      <el-menu-item index="/rightDataShow">权限分析</el-menu-item>
-      <!--      <el-menu-item index="3-3">选项3</el-menu-item>
-            <el-submenu index="3-4">
-              <template slot="title">选项4</template>
-              <el-menu-item index="3-4-1">选项4-1</el-menu-item>
-            </el-submenu>-->
-    </el-submenu>
 
-    <!--    数据统计-->
-    <el-submenu index="5">
-      <template slot="title">
-        <i :style="{color:iconColor}" class="el-icon-pie-chart"></i>
-        <span slot="title">数据统计</span>
-      </template>
-      <el-menu-item index="3-1">选项1</el-menu-item>
-      <el-menu-item index="3-2">选项2</el-menu-item>
-      <el-menu-item index="3-3">选项3</el-menu-item>
-      <el-submenu index="3-4">
-        <template slot="title">选项4</template>
-        <el-menu-item index="3-4-1">选项4-1</el-menu-item>
-      </el-submenu>
-    </el-submenu>
   </el-menu>
 </template>
 <script>
@@ -82,11 +33,22 @@ export default {
   data() {
     return {
       iconColor: '#c6e6e8',
+      rights: JSON.parse(sessionStorage.getItem("userInfo")) === null ? this.getInfo() : JSON.parse(sessionStorage.getItem("userInfo")).rights
     }
   },
   props: {
     isCollapse: Boolean,
   },
+  methods: {
+    getInfo() {
+      if (sessionStorage.getItem("userInfo") === null) {
+        this.$axios.get("/user/info").then(res => {
+          this.$store.commit("SET_USERINFO", res.data.data);
+          location.reload();
+        })
+      }
+    }
+  }
 
 }
 </script>
