5ce558f83966c325825e071ddb8bb4d037a2f762
WongSilver <783244553@qq.com>
2022-04-26 16:14:48 +0800

    fix: 修复用户信息界面不显示内容的问题

--- src/views/pages/UserInfo.vue
+++ src/views/pages/UserInfo.vue
@@ -2,16 +2,16 @@
   <el-card style="width: 400px;margin: 0 auto">
     <el-form :model="form" label-width="100px">
       <el-form-item label="用户名">
-        <el-input v-model="form.name" autocomplete="off"></el-input>
+        <el-input v-model="form.userName" autocomplete="off"></el-input>
       </el-form-item>
       <el-form-item label="手机号">
-        <el-input v-model="form.telephone" autocomplete="off"></el-input>
+        <el-input v-model="form.userTelephone" autocomplete="off"></el-input>
       </el-form-item>
       <el-form-item label="邮箱">
-        <el-input v-model="form.mail" autocomplete="off"></el-input>
+        <el-input v-model="form.userMail" autocomplete="off"></el-input>
       </el-form-item>
       <el-form-item label="班级">
-        <el-input v-model="form.groupId" autocomplete="off"></el-input>
+        <el-input v-model="form.userGroup" autocomplete="off"></el-input>
       </el-form-item>
     </el-form>
   </el-card>
@@ -26,6 +26,7 @@ export default {
     }
   },
   created() {
+    console.log(sessionStorage.getItem("userInfo"))
     this.form = JSON.parse(sessionStorage.getItem("userInfo"));
   }
 }
