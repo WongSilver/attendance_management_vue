bf360891dcf5da4cefccbeffb677948053edf977
WongSilver <783244553@qq.com>
2022-04-26 16:16:51 +0800

    feat: 添加显示所有用户功能

--- src/views/pages/Home.vue
+++ src/views/pages/Home.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <el-row style="margin-bottom: 40px;margin-bottom: 60px" :gutter="10">
+    <el-row style="margin-bottom: 60px" :gutter="10">
       <el-col :span="6">
         <el-card>
           <div style="color: #1ba784"><i class="el-icon-user"/> 师生总数</div>
           <div style="padding: 10px; font-weight: bold">
-            <el-tag>12345</el-tag>
+            <el-tag>{{ this.userTotal }}</el-tag>
           </div>
         </el-card>
       </el-col>
@@ -21,7 +21,7 @@
         <el-card>
           <div style="color:#f26b1f;"><i class="el-icon-link"/> 今日迟到人数</div>
           <div style="padding: 10px; font-weight: bold">
-            <el-tag type="warning">12345</el-tag>
+            <el-tag type="warning">3</el-tag>
           </div>
         </el-card>
       </el-col>
@@ -29,17 +29,17 @@
         <el-card>
           <div style="color:#c02c38;"><i class="el-icon-ship"/> 今日旷课人数</div>
           <div style="padding: 10px; font-weight: bold">
-            <el-tag type="danger">12345</el-tag>
+            <el-tag type="danger">12</el-tag>
           </div>
         </el-card>
       </el-col>
     </el-row>
     <el-row :gutter="10">
       <el-col :span="12">
-        <el-card id="main" style="width: 100%; height: 400px"></el-card>
+        <el-card id="lineMain" style="width: 100%; height: 400px"></el-card>
       </el-col>
       <el-col :span="12">
-        <el-card id="main1" style="width: 100%; height: 400px"></el-card>
+        <el-card id="pieMain" style="width: 100%; height: 400px"></el-card>
       </el-col>
     </el-row>
   </div>
@@ -51,43 +51,92 @@ import * as echarts from 'echarts';
 export default {
   name: "Home",
   data() {
-    return {}
+    return {
+      userTotal: 0,
+
+    }
   },
   //页面渲染后触发
   mounted() {
-    const chartDom = document.getElementById('main');
-    const myChart = echarts.init(chartDom);
-    const option = {
-          title: {
-            text: '每日请假人数统计图',
-            left: 'center',
-          },
-          tooltip: {
-            trigger: 'axis'
-          },
-          xAxis: {
-            type: 'category',
-            data: []
-          },
-          yAxis: {
-            type: 'value'
-          },
-          series: [
-            {
-              data: [],
-              type: 'line',
-              smooth: true
-            }
-          ]
+    const lineChartDom = document.getElementById('lineMain');
+    const pieChartDom = document.getElementById('pieMain');
+    const lineChart = echarts.init(lineChartDom);
+    const pieChart = echarts.init(pieChartDom);
+    const lineOption = {
+      title: {
+        text: '每日请假人数统计图',
+        left: 'center',
+      },
+      tooltip: {
+        trigger: 'axis'
+      },
+      xAxis: {
+        type: 'category',
+        data: []
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          data: [],
+          type: 'line',
+          smooth: true
         }
-    ;
-    this.$axios.get("/check/list").then(res => {
-      let line = res.data.data
-      option.xAxis.data = line.x
-      option.series[0].data = line.y
-      myChart.setOption(option);
-    })
-  }
+      ]
+    }
+    const pieOption = {
+      title: {
+        text: '每日请假人数统计图',
+        left: 'center'
+      },
+      tooltip: {
+        trigger: 'item'
+      },
+      legend: {
+        orient: 'vertical',
+        left: 'left'
+      },
+      series: [
+        {
+          name: '',
+          type: 'pie',
+          radius: '50%',
+          data: [],
+          emphasis: {
+            itemStyle: {
+              shadowBlur: 10,
+              shadowOffsetX: 0,
+              shadowColor: 'rgba(0, 0, 0, 0.5)'
+            }
+          }
+        }
+      ]
+    }
+    this.$axios.get("/check/homeList").then(res => {
+      let lineData = res.data.data
+      console.log()
+      let pieData = []
+      for (let i = 0; i < lineData.x.length; i++) {
+        let obj = {};
+        obj.name = lineData.x[i];
+        obj.value = lineData.y[i];
+        pieData[i] = obj
+      }
+      lineOption.xAxis.data = lineData.x
+      lineOption.series[0].data = lineData.y
+      pieOption.series[0].data = pieData
+      lineChart.setOption(lineOption);
+      pieChart.setOption(pieOption);
+    });
+
+    //获取学校总人数
+    this.$axios.get("/user/total").then(res => {
+      if (res.data.code === 200) {
+        this.userTotal = res.data.data;
+      }
+    });
+  },
 }
 </script>
 
