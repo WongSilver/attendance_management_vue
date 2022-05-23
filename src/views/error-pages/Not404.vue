743146ee70d3165d0ab1fcf19f54ba9e1465350a
WongSilver <783244553@qq.com>
2022-05-23 08:58:57 +0800

    feat: 新增页面无法找到404功能

---
+++ src/views/error-pages/Not404.vue
@@ -0,0 +1,13 @@
+<template>
+  <el-empty style="height:100%;line-height: 100%" description="404 找不到页面"></el-empty>
+</template>
+
+<script>
+export default {
+  name: "not404",
+}
+</script>
+
+<style scoped>
+
+</style>
